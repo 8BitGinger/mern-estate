0123f3307e8788febd0e3377f2532cff64474cc8
8BitGinger <ryan.fann>
2024-03-09 14:19:21 -0600

    updated api sign in route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,6 +11,30 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json('User Created');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ username });
+    if (!validUser) return next(errorHandler('User not found', 401));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler('Wrong Credentials', 401));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie('access_token', token, {
+        httpOnly: true,
+        sameSite: true,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      })
+      .status(200)
+
+      .json(rest);
   } catch (error) {
     next(error);
   }
